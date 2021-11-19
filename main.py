641116f22e240e3ca4d577859a90a5f12dbc9c8e
Arthur Méreur <arthur.mereur@gmail.com>
2021-11-19 11:39:33 +0100

    Ajout des fonctions de vérification

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
 # import
 
+nc = 0
+nl = 0
+plateau = []
+
+
+def find_player(joueur):
+    for x in range(nl):
+        for y in range(nc):
+            if plateau[x][y] == joueur:
+                return x, y
+
+
+def check_possible(joueur):
+    x, y = find_player(joueur)
+    if y + 1 < nc:
+        if plateau[x][y + 1] == 'O':
+            return True
+    if y - 1 >= 0:
+        if plateau[x][y - 1] == 'O':
+            return True
+    if x + 1 < nl:
+        if plateau[x + 1][y] == 'O':
+            return True
+    if x - 1 >= 0:
+        if plateau[x - 1][y] == 'O':
+            return True
+    if x + 1 < nl and y + 1 < nc:
+        if plateau[x + 1][y + 1] == 'O':
+            return True
+    if x + 1 < nl and y - 1 >= 0:
+        if plateau[x + 1][y - 1] == 'O':
+            return True
+    if x - 1 >= 0 and y - 1 >= 0:
+        if plateau[x - 1][y - 1] == 'O':
+            return True
+    if x - 1 >= 0 and y + 1 < nc:
+        if plateau[x - 1][y + 1] == 'O':
+            return True
+    return False
+
 
 def jeu():
     # Phase d'initialisation
-    nl = 0
-    nc = 0
 
     while nl <= 1:  # le nombre de lignes doit être supérieur à 1
         nl = int(input("Rentrez le nombre de lignes du plateau"))
@@ -13,7 +51,7 @@ def jeu():
     # le joueur doit choisir avec quel mode il veut jouer
     isComp = input("Si vous souhaitez jouer contre un ordinateur, envoyez 'y', sinon, envoyez n'importe quoi d'autre")
     computer = (isComp == 'y')
-    plateau = [[0 for x in range(nl)] for y in range(nc)]  # creation du plateau en fonction des paramètres
+    plateau = [[0 for _ in range(nl)] for _ in range(nc)]  # creation du plateau en fonction des paramètres
 
     # positionnement initial des pions
     if nl % 2 == 0:
@@ -22,6 +60,19 @@ def jeu():
     else:
         plateau[(nl - 1) / 2][0] = '1'
         plateau[(nl - 1) / 2][nc - 1] = '2'
+
+    # phase de jeu
+    joueur = 1
+    while check_possible(joueur):
+        i = 0
+
+        # TODO : déplacement
+
+        # TODO : destruction
+
+        # TODO : affichage
+
+        joueur = 2 / joueur  # permet de passer au joueur suivant sans utiliser un if
 
 
 if __name__ == '__main__':
