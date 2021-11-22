d40c1a335a790fb9e45f9ae255e3e41a209dfac2
Arthur Méreur <arthur.mereur@gmail.com>
2021-11-22 15:42:52 +0100

    ajout de l'affichage, debug, et de l'ordi

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import
+import random
 
 nc = 0
 nl = 0
@@ -8,7 +8,7 @@ plateau = []
 def find_player(joueur):
     for x in range(nl):
         for y in range(nc):
-            if plateau[x][y] == joueur:
+            if plateau[x][y] == str(joueur):
                 return x, y
 
 
@@ -38,13 +38,29 @@ def check_possible(joueur):
     if x - 1 >= 0 and y + 1 < nc:
         if plateau[x - 1][y + 1] == '0':
             return True
+
     return False
+
+
+def affichage():
+    print("\n")
+    for i in range(2 * nl + 1):
+        if (i % 2) == 0:
+            for j in range(nc):
+                print("__", end='')
+        else:
+            for j in range(2 * nc + 1):
+                if (j % 2) == 0:
+                    print("|", end='')
+                else:
+                    print(plateau[int((i - 1) / 2)][int((j - 1) / 2)], end='')
+        print("\n", end='')
 
 
 def jeu():
     # Phase d'initialisation
 
-    global nl, nc
+    global nl, nc, plateau
 
     while nl <= 1:  # le nombre de lignes doit être supérieur à 1
         nl = int(input("Rentrez le nombre de lignes du plateau"))
@@ -53,49 +69,73 @@ def jeu():
     # le joueur doit choisir avec quel mode il veut jouer
     isComp = input("Si vous souhaitez jouer contre un ordinateur, envoyez 'y', sinon, envoyez n'importe quoi d'autre")
     computer = (isComp == 'y')
-    plateau = [[0 for _ in range(nl)] for _ in range(nc)]  # creation du plateau en fonction des paramètres
+    plateau = [['0' for _ in range(nc)] for _ in range(nl)]  # creation du plateau en fonction des paramètres
 
     # positionnement initial des pions
     if nl % 2 == 0:
-        plateau[nl / 2][0] = '1'
-        plateau[nl / 2 - 1][nc - 1] = '2'
+        plateau[int(nl / 2)][0] = '1'
+        plateau[int(nl / 2 - 1)][nc - 1] = '2'
     else:
-        plateau[(nl - 1) / 2][0] = '1'
-        plateau[(nl - 1) / 2][nc - 1] = '2'
+        plateau[int((nl - 1) / 2)][0] = '1'
+        plateau[int((nl - 1) / 2)][nc - 1] = '2'
 
+    affichage()
     # phase de jeu
     joueur = 1
     while check_possible(joueur):
-        i = 0
+        if (joueur == 2) and computer:
+            print("C'est au tour de l'ordinateur")
+        else:
+            print("C'est au tour du joueur ", joueur)
 
         # déplacement
         deplacement_fait = False
-        while not deplacement_fait:
-            posx = int(input("Où voulez-vous placer votre pion en ligne ?"))
-            posy = int(input("Où voulez-vous plcer votre pion en colonne ?"))
-            x, y = find_player(joueur)
-            if abs(posy - y) <= 1 and abs(posx - x) <= 1:
-                if 0 <= posx < nl and 0 <= posy < nc:
-                    if plateau[posx][posy] == '0':
-                        plateau[x][y] = '0'
-                        plateau[posx][posy] = str(joueur)
-                        deplacement_fait = True
+        if (joueur == 1) or not computer:
+            while not deplacement_fait:
+                posx = int(input("Où voulez-vous placer votre pion en ligne ?"))
+                posy = int(input("Où voulez-vous plcer votre pion en colonne ?"))
+                x, y = find_player(joueur)
+                if abs(posy - y) <= 1 and abs(posx - x) <= 1:
+                    if 0 <= posx < nl and 0 <= posy < nc:
+                        if plateau[posx][posy] == '0':
+                            plateau[x][y] = '0'
+                            plateau[posx][posy] = str(joueur)
+                            deplacement_fait = True
+        else:
+            while not deplacement_fait:
+                posx = int(random.choice(range(nl)))
+                posy = int(random.choice(range(nc)))
+                x, y = find_player(joueur)
+                if abs(posy - y) <= 1 and abs(posx - x) <= 1:
+                    if 0 <= posx < nl and 0 <= posy < nc:
+                        if plateau[posx][posy] == '0':
+                            plateau[x][y] = '0'
+                            plateau[posx][posy] = str(joueur)
+                            deplacement_fait = True
 
         # destruction
         destruction_faite = False
-        while not destruction_faite:
-            posx = int(input("Quelle case voulez-vous détruire en ligne ?"))
-            posy = int(input("Quelle case voulez-vous détruire en colonne"))
-            if 0 <= posx < nl and 0 <= posy < nc:
-                if plateau[posx][posy] == '0':
-                    plateau[posx][posy] = 'X'
+        if (joueur == 1) or not computer:
+            while not destruction_faite:
+                posx = int(input("Quelle case voulez-vous détruire en ligne ?"))
+                posy = int(input("Quelle case voulez-vous détruire en colonne"))
+                if 0 <= posx < nl and 0 <= posy < nc:
+                    if plateau[posx][posy] == '0':
+                        plateau[posx][posy] = 'X'
+                        destruction_faite = True
+        else:
+            while not destruction_faite:
+                posx = int(random.choice(range(nl)))
+                posy = int(random.choice(range(nc)))
+                if 0 <= posx < nl and 0 <= posy < nc:
+                    if plateau[posx][posy] == '0':
+                        plateau[posx][posy] = 'X'
+                        destruction_faite = True
 
-                    destruction_faite = True
+        affichage()
 
-        # TODO : affichage
-
-        joueur = 2 / joueur  # permet de passer au joueur suivant sans utiliser un if
-    print("Le gagnant est le joueur :", 2 / joueur)
+        joueur = int(2 / joueur)  # permet de passer au joueur suivant sans utiliser un if
+    print("Le gagnant est le joueur :", int(2 / joueur))
 
 
 if __name__ == '__main__':
